03be8c9fc200e0be0ccb3e9f652fc5a164516caa
Antonio Pedro Ferreira <apsf88@gmail.com>
2022-03-22 03:40:32 -0300

    Updates

---
+++ _next/static/4xYnwDZB_XHoKuKjbU6h9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-3ec4db1268ca5f9fc744.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
